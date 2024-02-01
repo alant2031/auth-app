19721d0ae412781e2727b2ace33db6cc0143c7ea
tnk <tnk@gm.com>
2024-02-01 20:39:35 -0300

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
+import { Nunito } from 'next/font/google';
+import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
+import LoginButton from '@/components/auth/login-button';
+
+const font = Nunito({
+	subsets: ['latin'],
+	weight: ['600'],
+});
 
 export default function Home() {
 	return (
-		<div className="border-green-300 border-2 w-fit rounded">
-			<Button size="lg">Click</Button>
-		</div>
+		<main className="flex h-full flex-col items-center justify-center bg-sky-500">
+			<div className="space-y-6 text-center">
+				<h1
+					className={cn(
+						'text-6xl font-semibold text-white drop-shadow-md',
+						font.className,
+					)}
+				>
+					🔐 Auth
+				</h1>
+				<p className="text-white text-lg">A simple authentication servie</p>
+				<LoginButton>
+					<Button variant="secondary" size="lg">
+						Sign in
+					</Button>
+				</LoginButton>
+			</div>
+		</main>
 	);
 }
---
+++ components/auth/login-button.tsx
@@ -0,0 +1,31 @@
+'use client';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+interface LoginButtonProps {
+	children: React.ReactNode;
+	mode?: 'modal' | 'redirect';
+	asChild?: boolean;
+}
+
+function LoginButton({
+	children,
+	mode = 'redirect',
+	asChild,
+}: LoginButtonProps) {
+	const router = useRouter();
+	const handleClick = () => {
+		router.push('/auth/login');
+	};
+
+	if (mode == 'modal') {
+		return <span>TODO: Implement modal</span>;
+	}
+	return (
+		<span onClick={handleClick} className="cursor-pointer">
+			{children}
+		</span>
+	);
+}
+
+export default LoginButton;
