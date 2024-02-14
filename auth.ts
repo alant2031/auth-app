7cc1a54c411344ba6b91b67c007b6104b6b53c7d
tnk <tnk@gm.com>
2024-02-14 07:16:28 -0300

    Callbacks

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 
 import { db } from '@/lib/db';
 import authConfig from '@/auth.config';
+import { getUserById } from '@/data/user';
 
 export const {
 	handlers: { GET, POST },
@@ -11,6 +12,35 @@ export const {
 	signOut,
 } = NextAuth({
 	adapter: PrismaAdapter(db),
+	callbacks: {
+		async signIn({ user }) {
+			const existingUser = await getUserById(user.id!);
+
+			if (!existingUser || !existingUser.emailVerified) {
+				return false;
+			}
+
+			return true;
+		},
+		async session({ token, session }) {
+			console.log({ sessionToken: token });
+			if (token.sub && session.user) {
+				session.user.id = token.sub;
+			}
+
+			if (token.role && session.user) {
+				session.user.role = token.role;
+			}
+			return session;
+		},
+		async jwt({ token }) {
+			if (!token.sub) return token;
+			const existingUser = await getUserById(token.sub);
+			if (!existingUser) return token;
+			token.role = existingUser.role;
+			return token;
+		},
+	},
 	session: { strategy: 'jwt' },
 	...authConfig,
 });
