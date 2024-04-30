59592bf999d70fda0af189282e551746169f1fcf
tnk <tnk@gm.com>
2024-04-29 23:22:37 -0300

    Fix .env and email actions

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,9 +1,10 @@
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-	const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+	const confirmLink = domain + '/auth/new-verification?token=' + token;
 
 	await resend.emails.send({
 		from: 'onboarding@resend.dev',
@@ -14,7 +15,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-	const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+	const resetLink = domain + '/auth/new-password?token=' + token;
 
 	await resend.emails.send({
 		from: 'onboarding@resend.dev',
@@ -28,7 +29,7 @@ export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 	await resend.emails.send({
 		from: 'onboarding@resend.dev',
 		to: email,
-		subject: '2FA Code',
-		html: `<p>Your 2FA Code: ${token}<p/>`,
+		subject: 'Verificação em duas etapas',
+		html: `<p>Código 2FA: ${token}<p/>`,
 	});
 };
